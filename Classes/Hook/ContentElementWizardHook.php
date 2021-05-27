47ebe80f690a78e897c8de0b110973553a1a3643
Ingo Fabbri <inf@mmc-agentur.at>
2021-05-27 15:19:42 +0200

    [TASK] localize CE wizard title/description fields

--- Classes/Hook/ContentElementWizardHook.php
+++ Classes/Hook/ContentElementWizardHook.php
@@ -2,7 +2,6 @@
 
 namespace Typo3Api\Hook;
 
-
 use TYPO3\CMS\Backend\Wizard\NewContentElementWizardHookInterface;
 
 class ContentElementWizardHook implements NewContentElementWizardHookInterface
@@ -13,7 +12,7 @@ class ContentElementWizardHook implements NewContentElementWizardHookInterface
      * @param array $wizardItems Array of Wizard Items
      * @param \TYPO3\CMS\Backend\Controller\ContentElement\NewContentElementController $parentObject Parent object New Content element wizard
      */
-    public function manipulateWizardItems(&$wizardItems, &$parentObject)
+    public function manipulateWizardItems(&$wizardItems, &$parentObject): void
     {
         if (!isset($GLOBALS['TCA']['tt_content']['ctrl']['EXT']['typo3api']['content_elements'])) {
             return;
@@ -23,9 +22,13 @@ class ContentElementWizardHook implements NewContentElementWizardHookInterface
         $values = array_values($wizardItems);
 
         foreach ($GLOBALS['TCA']['tt_content']['ctrl']['EXT']['typo3api']['content_elements'] as $section => $contentElements) {
-            $sectionIndex = array_search($section, $keys);
+            $sectionIndex = array_search($section, $keys, true);
+            array_walk($contentElements, static function (&$contentElement) {
+                $contentElement['title'] = $GLOBALS['LANG']->sL($contentElement['title']);
+                $contentElement['description'] = $GLOBALS['LANG']->sL($contentElement['description']);
+            });
             array_splice($values, $sectionIndex + 1, 0, $contentElements);
-            array_splice($keys, $sectionIndex + 1, 0, array_map(function ($contentElement) use($section) {
+            array_splice($keys, $sectionIndex + 1, 0, array_map(static function ($contentElement) use ($section) {
                 return $section . '_' . $contentElement['CType'];
             }, $contentElements));
         }
