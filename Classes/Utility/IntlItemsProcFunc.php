eb45b3dcb4100c9d0463ff86b6617d9bd084bde9
Ingo Fabbri <inf@mmc-agentur.at>
2021-06-15 18:12:25 +0200

    [TASK] make compatible with symfony/intl ^5

--- Classes/Utility/IntlItemsProcFunc.php
+++ Classes/Utility/IntlItemsProcFunc.php
@@ -2,14 +2,14 @@
 
 namespace Typo3Api\Utility;
 
-
-use Symfony\Component\Intl\Intl;
+use Symfony\Component\Intl\Countries;
+use Symfony\Component\Intl\Languages;
 
 class IntlItemsProcFunc
 {
     public function addCountryNames(&$params): void
     {
-        $countryNames = Intl::getRegionBundle()->getCountryNames('en');
+        $countryNames = Countries::getNames();
         asort($countryNames);
         foreach ($countryNames as $countryCode => $countryName) {
             $params['items'][] = [$countryName, $countryCode];
@@ -18,7 +18,7 @@ class IntlItemsProcFunc
 
     public function addLanguages(&$params): void
     {
-        $languageNames = Intl::getLanguageBundle()->getLanguageNames('en');
+        $languageNames = Languages::getNames();
         asort($languageNames);
         foreach ($languageNames as $locale => $languageName) {
             $params['items'][] = [$languageName, $locale];
