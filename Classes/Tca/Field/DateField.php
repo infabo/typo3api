d5a3260802ee87773fb91a69603df25bcdb46bb8
Ingo Fabbri <inf@mmcagentur.at>
2024-10-30 14:45:05 +0100

    task: migrate datetime field
    see https://docs.typo3.org/c/typo3/cms-core/main/en-us/Changelog/12.0/Breaking-97358-RemovedEvalintFromTCATypeDatetime.html#breaking-97358-removed-eval-int-from-tca-type-datetime
    see https://docs.typo3.org/c/typo3/cms-core/main/en-us/Changelog/12.0/Feature-97232-NewTCATypeDatetime.html#feature-97232-new-tca-type-datetime

--- Classes/Tca/Field/DateField.php
+++ Classes/Tca/Field/DateField.php
@@ -17,11 +17,6 @@ class DateField extends AbstractField
         $resolver->setDefaults([
             'type' => 'date',
 
-            // in my tests, these 3 didn't work at all
-//            'default' => null,
-//            'min' => null,
-//            'max' => null,
-
             /**
              * Typo3 allows to use a date or datetime field in the database.
              * However, it is largely untested, I'd advise not to use it.
@@ -46,53 +41,16 @@ class DateField extends AbstractField
 
         $resolver->setAllowedValues('type', ['date', 'datetime', 'time', 'timesec']);
         $resolver->setAllowedTypes('useDateTime', 'bool');
-
-//        $timestampDate = function (Options $options, $value) {
-//            if (is_null($value)) {
-//                return null;
-//            }
-//
-//            if (is_int($value)) {
-//                return $value;
-//            }
-//
-//            if (is_string($value)) {
-//                return strtotime($value);
-//            }
-//
-//            if ($value instanceof \DateTimeInterface) {
-//                return $value->getTimestamp();
-//            }
-//
-//            $type = is_object($value) ? get_class($value) : gettype($value);
-//            throw new InvalidOptionsException("Don't know how to convert a date of type $type to a timestamp.");
-//        };
-//        $resolver->setNormalizer('min', $timestampDate);
-//        $resolver->setNormalizer('max', $timestampDate);
-//        $resolver->setNormalizer('default', $timestampDate);
     }
 
     public function getFieldTcaConfig(TcaBuilderContext $tcaBuilder): array
     {
         $config = [
-            'type' => 'input',
-            'renderType' => 'inputDateTime',
+            'type' => 'datetime',
+            'format' => $this->getOption('type'),
             'dbType' => $this->getOption('exposedDbType'),
-            'eval' => $this->getOption('type'),
             'range' => [],
         ];
-
-//        if ($this->getOption('default') !== null) {
-//            $config['default'] = $this->getOption('default');
-//        }
-//
-//        if ($this->getOption('max') !== null) {
-//            $config['range']['upper'] = $this->getOption('max');
-//        }
-//
-//        if ($this->getOption('min') !== null) {
-//            $config['range']['lower'] = $this->getOption('min');
-//        }
 
         return $config;
     }
