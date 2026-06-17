233ceaa85b12b5ac08581485d4a7852872cfbc7d
Ingo Fabbri <inf@mmcagentur.at>
2026-06-17 16:30:26 +0200

    fix(core): add TCA default columns to EnableColumnsConfiguration

--- Classes/Tca/EnableColumnsConfiguration.php
+++ Classes/Tca/EnableColumnsConfiguration.php
@@ -24,7 +24,13 @@ class EnableColumnsConfiguration implements TcaConfigurationInterface, DefaultTa
 
     public function getColumns(TcaBuilderContext $tcaBuilder): array
     {
-        return [];
+        return [
+            'hidden' => $GLOBALS['TCA']['tt_content']['columns']['hidden'],
+            'starttime' => $GLOBALS['TCA']['tt_content']['columns']['starttime'],
+            'endtime' => $GLOBALS['TCA']['tt_content']['columns']['endtime'],
+            'fe_group' => $GLOBALS['TCA']['tt_content']['columns']['fe_group'],
+            'editlock' => $GLOBALS['TCA']['tt_content']['columns']['editlock'],
+        ];
     }
 
     public function getPalettes(TcaBuilderContext $tcaBuilder): array
